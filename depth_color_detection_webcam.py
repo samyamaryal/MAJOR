fe7c1ec2c5279c551d77baf506bcd5067196842b
Samyam Aryal <69616491+samyamaryal@users.noreply.github.com>
2022-11-26 16:09:15 +0545

    Add files via upload
    Code containing depth map conversion and color detection.

---
+++ depth_color_detection_webcam.py
@@ -0,0 +1,172 @@
+import cv2
+import torch
+import time
+import numpy as np
+
+
+
+midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small", source = 'github', trust_repo = True)
+
+# Move model to GPU if available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+# Load transforms to resize and normalize the image
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms", source = 'github', trust_repo = True)
+
+transform = midas_transforms.small_transform
+
+
+# Open up the video capture from a webcam
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+
+    success, img = cap.read()
+
+    start = time.time()
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Apply input transforms
+    input_batch = transform(img).to(device)
+
+    # Prediction and resize to original resolution
+    with torch.no_grad():
+        prediction = midas(input_batch)
+
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+    depth_map = prediction.cpu().numpy()
+
+    depth_map = cv2.normalize(depth_map, None, 0, 1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
+
+
+    end = time.time()
+    totalTime = end - start
+
+    fps = 1 / totalTime
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    depth_map = (depth_map*255).astype(np.uint8)
+    depth_map = cv2.applyColorMap(depth_map , cv2.COLORMAP_HOT)
+
+    cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+    cv2.imshow('Image', img)
+    cv2.imshow('Depth Map', depth_map)
+    
+    # Reading the video from the 
+	# webcam in image frames 
+    imageFrame = depth_map
+    
+	# Convert the imageFrame in 
+	# BGR(RGB color space) to 
+	# HSV(hue-saturation-value) 
+	# color space 
+    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV) 
+    
+    
+    # THESE RANGES HAVE BEEN SET FOR A DIFFERENT COLOR MAP. RESET FOR 'HOT' COLOR FROM OPENCV.
+    # HOT DOESN'T HAVE BLUE COLOR. IT HAS RED AND ALL OTHER. YELLOW IS FOR NEAREST, GREEN MIDDLE OR SOMETHING.    
+    
+	# Set range for red color and 
+	# define mask 
+    red_lower = np.array([136, 87, 111], np.uint8) 
+    red_upper = np.array([180, 255, 255], np.uint8) 
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
+
+	# Set range for green color and 
+	# define mask 
+    green_lower = np.array([25, 52, 72], np.uint8) 
+    green_upper = np.array([102, 255, 255], np.uint8) 
+    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
+
+	# Set range for blue color and 
+	# define mask 
+    blue_lower = np.array([94, 80, 2], np.uint8) 
+    blue_upper = np.array([120, 255, 255], np.uint8) 
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
+	
+	# Morphological Transform, Dilation 
+	# for each color and bitwise_and operator 
+	# between imageFrame and mask determines 
+	# to detect only that particular color 
+    kernal = np.ones((5, 5), "uint8") 
+	
+	# For red color 
+    red_mask = cv2.dilate(red_mask, kernal) 
+    res_red = cv2.bitwise_and(imageFrame, imageFrame, 
+							mask = red_mask) 
+	
+	# For green color 
+    green_mask = cv2.dilate(green_mask, kernal) 
+    res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+								mask = green_mask) 
+	
+	# For blue color 
+    blue_mask = cv2.dilate(blue_mask, kernal) 
+    res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
+							mask = blue_mask) 
+
+	# Creating contour to track red color 
+    contours, hierarchy = cv2.findContours(red_mask, 
+										cv2.RETR_TREE, 
+										cv2.CHAIN_APPROX_SIMPLE) 
+	
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+									(x + w, y + h), 
+									(0, 0, 255), 2) 
+			
+            cv2.putText(imageFrame, "Red Colour", (x, y), 
+						cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+						(0, 0, 255))	 
+
+	# Creating contour to track green color 
+    contours, hierarchy = cv2.findContours(green_mask, 
+										cv2.RETR_TREE, 
+										cv2.CHAIN_APPROX_SIMPLE) 
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+									(x + w, y + h), 
+									(0, 255, 0), 2) 
+			
+            cv2.putText(imageFrame, "NEAR!", (x, y), 
+						cv2.FONT_HERSHEY_SIMPLEX, 
+						1.0, (0, 255, 0)) 
+
+	# Creating contour to track blue color 
+    contours, hierarchy = cv2.findContours(blue_mask, 
+										cv2.RETR_TREE, 
+										cv2.CHAIN_APPROX_SIMPLE) 
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+									(x + w, y + h), 
+									(255, 0, 0), 2) 
+			
+            cv2.putText(imageFrame, "Blue Colour", (x, y), 
+						cv2.FONT_HERSHEY_SIMPLEX, 
+						1.0, (255, 0, 0)) 
+			
+	# Program Termination 
+    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
+    if cv2.waitKey(10) & 0xFF == ord('q'): 
+        cap.release() 
+        cv2.destroyAllWindows()
+        break
